273f11aa8ff2c3db8313d070ac652c52350d09e6
JromarioSs <jromario2014@gmail.com>
2022-08-15 09:38:37 -0300

    RocketSeat Ignite desafio 2 feito

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(404).json({ error: "User not found!" });
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro) {
+    return next();
+  }
+
+  if(!user.pro && user.todos.length < 10) {
+    return next();
+  }
+
+  return response.status(403).json({ error: "You already have more than 10 todos!" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id: todoId } = request.params;
+
+  const userMatch = users.find(user => user.username === username);
+  
+  if (!userMatch) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+  
+  if(!validate(todoId)) {
+    return response.status(400).json({ error: "Id is not a uuid!" });
+  }
+
+  const idMatch = userMatch.todos.find(user => user.id === todoId);
+
+  if(!idMatch) {
+    return response.status(404).json({ error: "Id not match!" });
+  }
+
+  request.todo = idMatch;
+  request.user = userMatch;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  const userExist = users.find(user => user.id === id);
+
+  if(userExist) {
+    request.user = userExist;
+    return next();
+  }
+
+  return response.status(404).json({ error: "User not found!" });
 }
 
 app.post('/users', (request, response) => {
